d4160bbd70aee5d33aac8c54416891c0e3bf94c4
Christen Shubaly <shub0014@algonquinlive.com>
2023-03-24 00:46:14 -0400

    LocalStorage working with sign in and out

---
+++ localStorage.js
@@ -0,0 +1,25 @@
+const $signIn = document.getElementById('signIn')
+const $inOutBtn = document.getElementById('in-out')
+const $inOut = document.getElementById('inOutBtn')
+
+if ($signIn) {
+    $signIn.addEventListener("submit", function () {
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+       
+        window.localStorage.setItem('email', email)
+        window.localStorage.setItem('password', password)
+       
+    })
+}
+
+if (window.localStorage.getItem('email')){
+    $inOut.value = "Sign Out"
+}
+
+$inOutBtn.addEventListener("submit", function () {
+    if ($inOut.value === "Sign Out") {
+        window.localStorage.clear()
+    }
+})
+
